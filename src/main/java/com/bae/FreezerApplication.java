b5daaa6b3f478be18cee9d8d76bd612f3309060b
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2020-01-07 11:36:32 +0000

    changing application

--- src/main/java/com/bae/FreezerApplication.java
+++ src/main/java/com/bae/FreezerApplication.java
@@ -2,12 +2,21 @@ package com.bae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FreezerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FreezerApplication.class, args);
+public class FreezerApplication extends SpringBootServletInitializer {
+		 
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(FreezerApplication.class);
+	    }
+	 
+	    public static void main(String[] args) {
+	        SpringApplication.run(FreezerApplication.class, args);
+	    }
+	
 	}
 
 } 
