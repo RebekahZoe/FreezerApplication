7b1fa609b441c7173e64552a9ac54667f55b433c
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2020-01-08 15:37:22 +0000

    adding front end to repo

---
+++ src/main/resources/static/js/Freezers.js
@@ -0,0 +1,107 @@
+"use strict";
+
+function getFreezers(){
+    axios.get('http://localhost:8080/getAllFreezers')
+    .then((response)=>{
+        showFreezers(response.data);
+        console.log(response.data);
+    }).catch((error)=>{
+        console.error(error);
+    })
+}
+
+const freezerList = document.getElementById("listOfFreezers");
+function showFreezers(freezer){
+freezerList.innerHTML="";
+for (let f of freezer){
+    const newFreezer = document.createElement("li");
+    newFreezer.innerHTML = f.freezerName;
+
+    freezerList.appendChild(newFreezer);
+    newFreezer.addEventListener('click', () => redirect(f.freezerName,f.id));
+}
+}
+function redirect(name,id){
+    window.location.href = "\IndividualFreezer.html?name="+name+"&id="+id;
+}
+
+function createFreezer(){
+    var hasNumber = /\d/;
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    const freezer = document.getElementById("freezerNameAdd").value;
+    if(freezer !== ""){
+        if (hasNumber.test(freezer)){
+            alert("Please enter a valid freezer name (No numbers)");
+        } 
+        else if (format.test(freezer)){
+            alert("Please enter a valid freezer name (No special characters)");
+        }
+        else if (freezer.length < 6){
+            alert("Please enter a valid freezer name (must be greater than 6 characters)");
+        }
+        else{
+            console.log(freezer);
+            axios.post('http://localhost:8080/createFreezer',{
+                freezerName : freezer
+            })
+            .then(function (response) {
+                console.log(response);
+                getFreezers();
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+              getFreezers();
+              
+        }
+        document.getElementById("freezerNameAdd").value="";
+    }
+    else{
+        alert("Please enter a valid freezer name");
+    }
+
+}
+
+
+
+function deleteValidation(){
+    const freezer = document.getElementById("freezerNameDelete").value;
+    var hasNumber = /\d/;
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if(freezer !== ""){
+        console.log(freezer);
+        if (hasNumber.test(freezer)){
+            alert("Please enter a valid freezer name (No numbers)");
+        } 
+        else if (format.test(freezer)){
+            alert("Please enter a valid freezer name (No special characters)");
+        }
+        else if (freezer.length < 6){
+            alert("Please enter a valid freezer name (must be greater than 6 characters)");
+        }
+        else{
+            console.log(freezer);
+            if (confirm("Are you sure you want to delete this freezer?")){
+                axios.delete("http://localhost:8080/deleteFreezerByName/"+freezer)
+                .then((response)=>{
+                console.log(response);
+                getFreezers();
+
+                })
+                .catch(function (error) {
+                console.log(error);
+                getFreezers();
+
+                });
+                freezer.value= "";
+            }
+    }
+
+    }
+    else{
+        alert("Please enter a valid freezer name");
+    }
+}
+
+
---
+++ src/main/resources/static/js/IndividualFreezer.js
@@ -0,0 +1,210 @@
+"use strict";
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+function setTitle(){
+    var title = document.getElementById("freezerName");
+    var newTitle = document.createElement("h1");
+    var titleText = getQueryVariable("name");
+    newTitle.innerHTML = decodeURI(titleText);
+    title.appendChild(newTitle);
+
+}
+var fId = getQueryVariable("id");
+
+function getItemsFromFreezer(){
+    console.log("bloop");
+   axios.get("http://localhost:8080/getItemsFromFreezer/"+fId)
+   .then((response)=>{
+        showItemsFromFreezer(response.data);
+        console.log(response);
+   })
+   .catch((error)=>{
+       console.error(error);
+   })
+}
+
+const headerRow = document.getElementById("headerRow")
+const itemList = document.getElementById("itemsInFreezer");
+function showItemsFromFreezer(items){
+    itemList.innerHTML = " ";
+    headerRow.innerHTML =" ";
+    var itemHeader = document.createElement("th");
+    var quantityHeader = document.createElement("th");
+
+    itemHeader.innerHTML = "Items";
+    quantityHeader.innerHTML = "Quantity";
+
+    headerRow.appendChild(itemHeader);
+    headerRow.appendChild(quantityHeader);
+    itemList.appendChild(headerRow);
+
+    console.log(items);
+for (let item of items){
+    var newRow= document.createElement("tr");
+    var newItem = document.createElement("td");
+    var newQuantity = document.createElement("td");
+    newItem.innerHTML = item.itemName;
+    newQuantity.innerHTML = item.quantity;
+    newRow.appendChild(newItem);
+    newRow.appendChild(newQuantity);
+
+    itemList.appendChild(newRow);
+}
+}
+
+function createNewItem(){
+    
+    var hasNumber = /\d/;
+    var format = /[!@#$%^*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    const item = document.getElementById("itemNameAdd").value;
+    const quantity = document.getElementById("quantityAdd").value;
+    var message;
+    
+    if(item !== "" && quantity !==""){
+        if (hasNumber.test(item)){
+            message = "Please enter a valid item name (No numbers)" + quantityValidation(quantity);
+            alert(message);
+        } 
+        else if (format.test(item)){
+            message = "Please enter a valid item name (No special characters (& is allowed))" + quantityValidation(quantity);
+            alert(message);
+        }
+        else if (item.length < 3){
+            message = "Please enter a valid item name (must be greater than 3 characters)" + quantityValidation(quantity)
+            alert(message);
+        }
+        else{
+            message = quantityValidation(quantity);
+            if (message === " "){
+                axios.patch("http://localhost:8080/addItem/"+fId,{
+                    itemName : item,
+                    quantity : quantity
+                }).then(()=>{
+                    getItemsFromFreezer();
+                })
+                
+            }
+            else{
+                alert(message);
+            }
+        }
+        
+    }
+    else{
+        alert("Please enter a valid item name and quantity");
+    }
+
+
+}
+
+function quantityValidation(quantity){
+    var letters = /^[A-Za-z]+$/;
+    var format = /[!@#$%&^*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (letters.test(quantity)){
+        return " Please enter a valid quantity (no letters)"
+    }
+    else if (format.test(quantity)){
+        return " Please enter a valid quantity (no special characters)"
+    }
+    else if (quantity === "0"){
+        return " Please enter a valid quantity (quantity must be greater than 0)"
+    }
+    else{
+        return " "
+    }
+
+
+}
+function editItem(){
+    
+    var hasNumber = /\d/;
+    var format = /[!@#$%^*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    const item = document.getElementById("itemNameEdit").value;
+    const quantity = document.getElementById("quantityEdit").value;
+    var message;
+    
+    if(item !== "" && quantity !==""){
+        if (hasNumber.test(item)){
+            message = "Please enter a valid item name (No numbers)" + quantityValidation(quantity);
+            alert(message);
+        } 
+        else if (format.test(item)){
+            message = "Please enter a valid item name (No special characters (& is allowed))" + quantityValidation(quantity);
+            alert(message);
+        }
+        else if (item.length < 3){
+            message = "Please enter a valid item name (must be greater than 3 characters)" + quantityValidation(quantity)
+            alert(message);
+        }
+        else{
+            message = quantityValidation(quantity);
+            if (message === " "){
+                axios.put("http://localhost:8080/updateItemByName/"+item,{
+                    itemName : item,
+                    quantity : quantity
+                }).then(()=>{
+                    getItemsFromFreezer();
+                })
+                
+            }
+            else{
+                alert(message);
+            }
+        }
+        
+    }
+    else{
+        alert("Please enter a valid item name and quantity");
+    }
+}
+
+function deleteItem(){
+    var hasNumber = /\d/;
+    var format = /[!@#$%^*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    const item = document.getElementById("itemNameDelete").value;
+    var message;
+    
+    if(item !== ""){
+        if (hasNumber.test(item)){
+            message = "Please enter a valid item name (No numbers)";
+            alert(message);
+        } 
+        else if (format.test(item)){
+            message = "Please enter a valid item name (No special characters (& is allowed))";
+            alert(message);
+        }
+        else if (item.length < 3){
+            message = "Please enter a valid item name (must be greater than 3 characters)"; 
+            alert(message);
+        }
+        else{ 
+            if (confirm("Are you sure you want to delete this freezer?")){
+                axios.delete("http://localhost:8080/deleteItemFromFreezerByName/"+item)               
+                .then((response)=>{
+                    console.log(response);
+                    getItemsFromFreezer();
+                })
+                
+            }
+        }
+            
+        }
+        
+    
+    else{
+        alert("Please enter a valid item name");
+    }
+}
+
+
