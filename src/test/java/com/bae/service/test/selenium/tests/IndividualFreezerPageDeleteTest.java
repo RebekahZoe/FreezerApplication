12080502db51c55f558af39880f36de759b2fa04
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2020-01-14 09:35:15 +0000

    edit test works

---
+++ src/test/java/com/bae/service/test/selenium/tests/IndividualFreezerPageDeleteTest.java
@@ -0,0 +1,14 @@
+package com.bae.service.test.selenium.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IndividualFreezerPageDeleteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
